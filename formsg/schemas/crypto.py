25525747c82871bd8f91673c93cd34a2236a070c
Seah Chin Ying <scy.6char@gmail.com>
2022-02-03 20:49:27 +0800

    chore: define schemas for crypto

--- formsg/schemas/crypto.py
+++ formsg/schemas/crypto.py
@@ -1,4 +1,14 @@
+from marshmallow import Schema, fields
 from typing_extensions import TypedDict
+
+from formsg.util.crypto import FieldType, FormField
 
 EncryptedFile = TypedDict("EncryptedFile", {})
 EncryptedContent = TypedDict("EncryptedContent", {})
+
+
+class DecryptParamsSchema(Schema):
+    encryptedContent = fields.Str(required=True)
+    version = fields.Number(required=True)
+    verifiedContent = fields.Str()
+    attachmentDownloadUrls = fields.Dict()
---
+++ formsg/schemas/verification.py
@@ -0,0 +1,17 @@
+from typing_extensions import TypedDict
+
+
+VerificationAuthenticateOptions = TypedDict(
+    "VerificationAuthenticateOptions",
+    {
+        "signatureString": str,
+        "submissionCreatedAt": int,
+        "fieldId": str,
+        "answer": str,
+        "publicKey": str,
+    },
+)
+
+VerificationSignatureSchema = TypedDict(
+    "VerificationSignatureSchema", {"v": str, "t": int, "s": str, "f": str}
+)
